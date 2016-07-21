907e4e6ffca1ce17f1fe1c892de0484c3ce38e51
Luigi Serra <luigser@gmail.com>
2016-07-21 12:06:01 +0200

    updates

--- static/scripts/ethersheet/config.js
+++ static/scripts/ethersheet/config.js
@@ -8,8 +8,8 @@ module.exports = {
   db_host: 'localhost',
   db_type: 'mysql',
   debug: false,
-  default_row_count: 0,
-  default_col_count: 0,
+  default_row_count: 10,
+  default_col_count: 10,
   expire_days: 0,
   intro_text: "Welcome to SPOD Spreadsheet, enter a sheet name to get started"
 }
--- static/scripts/ethersheet/ethersheet_service.js
+++ static/scripts/ethersheet/ethersheet_service.js
@@ -120,8 +120,7 @@ EtherSheetService.prototype.sheetToCSV = function(sheet_id,cb){
     _.each(sheet_data.rows, function(row){
       _.each(sheet_data.cols, function(col){
         if(sheet_data.cells[row] && sheet_data.cells[row][col]){
-          //output += sheet_data.cells[row][col].value + ',';
-          output += '"' +  sheet_data.cells[row][col].value.replace('"','""') + '",';
+          output += sheet_data.cells[row][col].value + ',';
         } else {
           output += ',';
         }
