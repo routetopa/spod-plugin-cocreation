29c2472c6c1087f399b782546e42ded834c02711
Luigi Serra <luigser@gmail.com>
2016-07-21 12:10:36 +0200

    updates

--- static/scripts/ethersheet/install_ethersheet.sh
+++ static/scripts/ethersheet/install_ethersheet.sh
@@ -62,7 +62,7 @@ copySettings()
     cp ethersheet_service.js /home/ethersheet/ethersheet/EtherSheet/lib
     cp ethersheet.js /home/ethersheet/ethersheet/EtherSheet/node_modules/es_client
     cp templates.js /home/ethersheet/ethersheet/EtherSheet/node_modules/es_client
-    mkdir /home/ethersheet/ethersheet/EtherSheet/node_modules/es_client/it
+    mkdir /home/ethersheet/ethersheet/EtherSheet/node_modules/es_client/locales/it
     cp -R ./es_client/* /home/ethersheet/ethersheet/EtherSheet/node_modules/es_client
     cp ./views/table.js /home/ethersheet/ethersheet/EtherSheet/node_modules/es_client/views
     cp ./views/cell_menu.js /home/ethersheet/ethersheet/EtherSheet/node_modules/es_client/views
