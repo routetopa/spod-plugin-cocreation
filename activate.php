f542c6fc7bd3077be41cdd7f8c07efe2fc399949
andrpet <andrpet@gmail.com>
2017-11-30 10:53:39 +0100

    update

--- activate.php
+++ activate.php
@@ -1,6 +1,9 @@
 <?php
 
-OW::getPluginManager()->addPluginSettingsRouteName('cocreation', 'cocreation-settings');
-
+/*OW::getPluginManager()->addPluginSettingsRouteName('cocreation', 'cocreation-settings');
 OW::getNavigation()->addMenuItem(OW_Navigation::MAIN, 'cocreation.index', 'cocreation', 'main_menu_item', OW_Navigation::VISIBLE_FOR_ALL);
-BOL_LanguageService::getInstance()->importPrefixFromZip(OW::getPluginManager()->getPlugin('cocreation')->getRootDir() . 'langs.zip', 'cocreation');
+BOL_LanguageService::getInstance()->importPrefixFromZip(OW::getPluginManager()->getPlugin('cocreation')->getRootDir() . 'langs.zip', 'cocreation');*/
+
+$authorization = OW::getAuthorization();
+$groupName = 'cocreation';
+$authorization->addAction($groupName, 'Publish on CKAN');
