a4dd253871c61e748192c45b63cd59e2273cbcef
andrpet <andrpet@gmail.com>
2017-11-30 10:52:04 +0100

    update

--- uninstall.php
+++ uninstall.php
@@ -1,14 +1,3 @@
 <?php
 
 OW::getRouter()->removeRoute('cocreationep.index');
-
-/*OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_room`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_room_doc`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_room_member`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_template`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_room_datalet`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_room_dataset`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_room_postit`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_room_sheet`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_room_metadata`;");
-OW::getDbo()->query("DROP TABLE `" . OW_DB_PREFIX . "cocreation_dataset`;");*/
