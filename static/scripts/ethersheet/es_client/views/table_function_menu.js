a69fbc613b4ac4b66d2f15f90007f97e26eb05b0
Luigi Serra <luigser@gmail.com>
2018-01-23 15:57:37 +0100

    Updates

--- static/scripts/ethersheet/es_client/views/table_function_menu.js
+++ static/scripts/ethersheet/es_client/views/table_function_menu.js
@@ -125,9 +125,10 @@ define( function(require,exports,module){
             var current_cell_keys = this.current_results[Object.keys(this.current_results)[this.current_selected_result - 1]];
             this.pageSelection(this.getPageForRow(current_cell_keys.row_id));
             //scroll to the cell related to the current result
-            //var cell = $('td[data-row_id="' + current_cell_keys.row_id + '"][data-col_id="' + current_cell_keys.col_id + '"]');
-            var cell = $('td#'+ current_cell_keys.row_id + '-' + current_cell_keys.col_id);
-            this.scrollTo(cell);
+            _.debounce(function(){
+                var cell = $('td[data-row_id="' + current_cell_keys.row_id + '"][data-col_id="' + current_cell_keys.col_id + '"]');
+                this.scrollTo(cell);
+            }.bind(this), 300);
         },
 
         getNewResult: function(e){
