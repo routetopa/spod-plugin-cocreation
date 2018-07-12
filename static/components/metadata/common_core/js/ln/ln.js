f526c9a401314856eeee1c6e6cba8a65b60e359b
andrpet <andrpet@gmail.com>
2018-07-12 17:04:47 +0200

    update

---
+++ static/components/metadata/common_core/js/ln/ln.js
@@ -0,0 +1,205 @@
+common_core_ln = [];
+
+/******** IT ********/
+
+/* common_core_required_fields */
+common_core_ln["common_core_required_fields-title-it"] = '"Common Core" Required Fields';
+common_core_ln["common_core_if_applicable_fields-title-it"] = '"Common Core" Required if Applicable Fields';
+
+/* title */
+common_core_ln["title-it"] = "title";
+common_core_ln["title-label-it"] = "Titolo";
+common_core_ln["title-placeholder-it"] = "Titolo";
+common_core_ln["title-tooltip-it"] = "Nome dell'asset. Dovrebbe essere in Italiano/Inglese ed includere sufficienti dettagli per facilitare le operazioni di ricerca.";
+
+/* description */
+common_core_ln["description-it"] = "description";
+common_core_ln["description-label-it"] = "Descrizione";
+common_core_ln["description-placeholder-it"] = "Descrizione ";
+common_core_ln["description-tooltip-it"] = "Descrizione (ad esempio un piccolo riassunto) con sufficienti dettagli necessari all'utente per capire velocemente se l'asset è di suo interesse.";
+
+/* license */
+common_core_ln["license-it"] = "license";
+common_core_ln["license-label-it"] = "Licenza";
+common_core_ln["license-placeholder-it"] = "Licenza";
+common_core_ln["license-tooltip-it"] = "La licenza con la quale viene rilasciato il dataset. Fare riferimento a Open License per ulteriori informazioni.";
+
+/* language */
+common_core_ln["language-it"] = "language";
+common_core_ln["language-label-it"] = "Lingua";
+common_core_ln["language-placeholder-it"] = "Lingua";
+common_core_ln["language-tooltip-it"] = "Lingua del dataset.";
+
+/* version */
+common_core_ln["version-it"] = "version";
+common_core_ln["version-label-it"] = "Versione";
+common_core_ln["version-placeholder-it"] = "Versione";
+common_core_ln["version-tooltip-it"] = "Versione del dataset.";
+
+/* contact_name */
+common_core_ln["contact_name-it"] = "contact_name";
+common_core_ln["contact_name-label-it"] = "Contatto";
+common_core_ln["contact_name-placeholder-it"] = "Contatto";
+common_core_ln["contact_name-tooltip-it"] = "Contatto personale per questo dataset.";
+
+/* contact_email */
+common_core_ln["contact_email-it"] = "contact_email";
+common_core_ln["contact_email-label-it"] = "Email";
+common_core_ln["contact_email-placeholder-it"] = "Email";
+common_core_ln["contact_email-tooltip-it"] = "Email della persona responsabile per questo dataset.";
+
+/* maintainer */
+common_core_ln["maintainer-it"] = "maintainer";
+common_core_ln["maintainer-label-it"] = "Manutentore";
+common_core_ln["maintainer-placeholder-it"] = "Manutentore";
+common_core_ln["maintainer-tooltip-it"] = "Manutentore.";
+
+/* maintainer_email */
+common_core_ln["maintainer_email-it"] = "maintainer_email";
+common_core_ln["maintainer_email-label-it"] = "E-mail del Manutentore";
+common_core_ln["maintainer_email-placeholder-it"] = "E-mail del Manutentore";
+common_core_ln["maintainer_email-tooltip-it"] = "E-mail del Manutentore.";
+
+/* origin */
+common_core_ln["origin-it"] = "origin";
+common_core_ln["origin-label-it"] = "Origine";
+common_core_ln["origin-placeholder-it"] = "Origine";
+common_core_ln["origin-tooltip-it"] = "Origine del dataset.";
+
+/* tags */
+common_core_ln["tags-it"] = "tags";
+common_core_ln["tags-label-it"] = "Tags";
+common_core_ln["tags-placeholder-it"] = "Tags";
+common_core_ln["tags-tooltip-it"] = "Tags che aiutino le operazioni di ricerca basate su parole chiave; includere termini tecnici e non.";
+
+/* last_update */
+common_core_ln["last_update-it"] = "last_update";
+common_core_ln["last_update-label-it"] = "Ultimo aggiornamento";
+common_core_ln["last_update-placeholder-it"] = "Ultimo aggiornamento";
+common_core_ln["last_update-tooltip-it"] = "La data dell'ultima volta in cui il dataset è stato cambiato, modificato o aggiornato.";
+
+/* publisher */
+common_core_ln["publisher-it"] = "publisher";
+common_core_ln["publisher-label-it"] = "Editore";
+common_core_ln["publisher-placeholder-it"] = "Editore";
+common_core_ln["publisher-tooltip-it"] = "L'editore ed, eventualmente, l'organizzazione a cui afferisce.";
+
+/* unique_identifier */
+common_core_ln["unique_identifier-it"] = "unique_identifier";
+common_core_ln["unique_identifier-label-it"] = "Identificatore univoco";
+common_core_ln["unique_identifier-placeholder-it"] = "Identificatore univoco";
+common_core_ln["unique_identifier-tooltip-it"] = "Un identificatore univoco per il dataset o per l'API gestita all'interno di un catalogo o di un database.";
+
+/* public_access_level */
+common_core_ln["public_access_level-it"] = "public_access_level";
+common_core_ln["public_access_level-label-it"] = "Livello di accesso pubblico";
+common_core_ln["public_access_level-placeholder-it"] = "Livello di accesso pubblico";
+common_core_ln["public_access_level-tooltip-it"] = "Il livello di accesso al dataset. Scegliere fra : pubblico (il dataset è disponibile a tutti senza restrizioni), pubblico ristretto (il dataset è pubblicamente disponibile a determinate condizioni), non pubblico (accessibile esclusivamente privatamente).";
+
+/* bureau_code */
+common_core_ln["bureau_code-it"] = "bureau_code";
+common_core_ln["bureau_code-label-it"] = "Codice Bureau";
+common_core_ln["bureau_code-placeholder-it"] = "Codice Bureau";
+common_core_ln["bureau_code-tooltip-it"] = "Codice gestito da agenzie federali come indicato dalla circolare A-11 OMB, Appendice C nel formato 015:11.";
+
+/* program_code */
+common_core_ln["program_code-it"] = "program_code";
+common_core_ln["program_code-label-it"] = "Codice programma";
+common_core_ln["program_code-placeholder-it"] = "Codice programma";
+common_core_ln["program_code-tooltip-it"] = "Codice del programma primario (dal registro federale dei programmi) relativo al dataset . Utilizzare il formato 015:001.";
+
+/* access_level_comment */
+common_core_ln["access_level_comment-it"] = "access_level_comment";
+common_core_ln["access_level_comment-label-it"] = "Commento al livello di accesso";
+common_core_ln["access_level_comment-placeholder-it"] = "Commento al livello di accesso";
+common_core_ln["access_level_comment-tooltip-it"] = "Fornire una motivazione per il livello di accesso selezionato in precedenza e istruzioni su come accedere al dataset qualora fosse riservato.";
+
+/* download_url */
+common_core_ln["download_url-it"] = "download_url";
+common_core_ln["download_url-label-it"] = "Download URL";
+common_core_ln["download_url-placeholder-it"] = "Download URL";
+common_core_ln["download_url-tooltip-it"] = "URL da cui scaricare direttamentte una versione del dataset.";
+
+/* endpoint */
+common_core_ln["endpoint-it"] = "endpoint";
+common_core_ln["endpoint-label-it"] = "Indirizzo";
+common_core_ln["endpoint-placeholder-it"] = "Indirizzo";
+common_core_ln["endpoint-tooltip-it"] = "Indirizzo del web service per l'accesso al dataset.";
+
+/* format */
+common_core_ln["format-it"] = "format";
+common_core_ln["format-label-it"] = "Formato";
+common_core_ln["format-placeholder-it"] = "Formato";
+common_core_ln["format-tooltip-it"] = "Il formato del file o dell'API del dataset.";
+
+/* spatial */
+common_core_ln["spatial-it"] = "spatial";
+common_core_ln["spatial-label-it"] = "Informazioni spaziali";
+common_core_ln["spatial-placeholder-it"] = "Informazioni spaziali";
+common_core_ln["spatial-tooltip-it"] = "La regione interessata dal dataset. Indicare la regione geografica di interesse.";
+
+/* temporal */
+common_core_ln["temporal-it"] = "temporal";
+common_core_ln["temporal-label-it"] = "Informazioni temporali";
+common_core_ln["temporal-placeholder-it"] = "Informazioni temporali";
+common_core_ln["temporal-tooltip-it"] = "L'intervallo temporale di validità del dataset (data di inizio e fine, se applicabile).";
+
+/* category */
+common_core_ln["category-it"] = "category";
+common_core_ln["category-label-it"] = "Categoria";
+common_core_ln["category-placeholder-it"] = "Categoria";
+common_core_ln["category-tooltip-it"] = "Principale categoria tematica del dataset.";
+
+/* data_dictionary */
+common_core_ln["data_dictionary-it"] = "data_dictionary";
+common_core_ln["data_dictionary-label-it"] = "Ontologia";
+common_core_ln["data_dictionary-placeholder-it"] = "Ontologia";
+common_core_ln["data_dictionary-tooltip-it"] = "Ontologia";
+
+/* data_quality */
+common_core_ln["data_quality-it"] = "data_quality";
+common_core_ln["data_quality-label-it"] = "Qualità dei dati";
+common_core_ln["data_quality-placeholder-it"] = "Qualità dei dati";
+common_core_ln["data_quality-tooltip-it"] = "Indicare se il dataset è conforme alle specifiche di qualità dei dati (si/no).";
+
+/* distribution */
+common_core_ln["distribution-it"] = "distribution";
+common_core_ln["distribution-label-it"] = "Distribuzione";
+common_core_ln["distribution-placeholder-it"] = "Distribuzione";
+common_core_ln["distribution-tooltip-it"] = "Indicare se il dataset è composto da più sorgenti e fornirne gli indirizzi.";
+
+/* frequency */
+common_core_ln["frequency-it"] = "frequency";
+common_core_ln["frequency-label-it"] = "Frequenza";
+common_core_ln["frequency-placeholder-it"] = "Frequenza";
+common_core_ln["frequency-tooltip-it"] = "Frequenza di pubblicazione del dataset.";
+
+/* homepage_url */
+common_core_ln["homepage_url-it"] = "homepage_url";
+common_core_ln["homepage_url-label-it"] = "URL della Homepage";
+common_core_ln["homepage_url-placeholder-it"] = "URL della Homepage";
+common_core_ln["homepage_url-tooltip-it"] = "Home page del dataset o dell'API che rende disponibile il dataset.";
+
+/* primary_it_investment_uii */
+common_core_ln["primary_it_investment_uii-it"] = "primary_it_investment_uii";
+common_core_ln["primary_it_investment_uii-label-it"] = "Primary IT investment UII";
+common_core_ln["primary_it_investment_uii-placeholder-it"] = "Primary IT investment UII";
+common_core_ln["primary_it_investment_uii-tooltip-it"] = "Informazioni per collegare il dataset alle fonti di investimento IT Unique Investment Identifier (UII).";
+
+/* related_documents */
+common_core_ln["related_documents-it"] = "related_documents";
+common_core_ln["related_documents-label-it"] = "Documenti correlati";
+common_core_ln["related_documents-placeholder-it"] = "Documenti correlati";
+common_core_ln["related_documents-tooltip-it"] = "Documenti correlati (documentazione tecnica, di sviluppo ecc ecc).";
+
+/* release_date */
+common_core_ln["release_date-it"] = "release_date";
+common_core_ln["release_date-label-it"] = "Data di rilascio";
+common_core_ln["release_date-placeholder-it"] = "Data di rilascio";
+common_core_ln["release_date-tooltip-it"] = "Data formale di rilascio.";
+
+/* system_of_records */
+common_core_ln["system_of_records-it"] = "system_of_records";
+common_core_ln["system_of_records-label-it"] = "Sistema di records";
+common_core_ln["system_of_records-placeholder-it"] = "Sistema di records";
+common_core_ln["system_of_records-tooltip-it"] = "Se il sistema è strutturato come un sistema di records secondo il Privacy Act del 1974, fornire un URL al sistema di Record collegato a questo dataset.";
